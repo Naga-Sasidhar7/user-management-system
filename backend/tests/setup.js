730375fa96b50abb85ccc667e2772a6001d0f465
Naga-Sasidhar7 <atchanagasasidhar@gmail.com>
2025-12-30 15:33:48 +0530

    Update setup.js

--- backend/tests/setup.js
+++ backend/tests/setup.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
-
 let mongoServer;
-
-// Before all tests
 beforeAll(async () => {
-  // Use in-memory MongoDB for testing
   mongoServer = await MongoMemoryServer.create();
   const mongoUri = mongoServer.getUri();
   
@@ -14,14 +10,10 @@ beforeAll(async () => {
     useUnifiedTopology: true,
   });
 });
-
-// After all tests
 afterAll(async () => {
   await mongoose.disconnect();
   await mongoServer.stop();
 });
-
-// Clear database before each test
 beforeEach(async () => {
   const collections = mongoose.connection.collections;
   
